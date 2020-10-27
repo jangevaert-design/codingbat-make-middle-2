c0a60dbe05721519d7fc97b329440cdb3fed05fe
Jan Gevaert <jan.gevaert@gmail.com>
2020-10-27 06:02:33 -0600

    Initial commit

---
+++ src/MakeMiddle2.java
@@ -0,0 +1,8 @@
+public class MakeMiddle2 {
+  public int[] makeMiddle(int[] nums) {
+    int[] result = new int[2];
+    result[0] = nums[(nums.length / 2) - 1];
+    result[1] = nums[nums.length / 2];
+    return result;
+  }
+}
